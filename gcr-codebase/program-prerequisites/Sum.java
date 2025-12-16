a0c842125d723c9f3b7b128534cf2c5edd9fb7a5
harshita584 <harshitapatel472@gmail.com>
2025-12-16 17:04:12 +0530

    [Harshita Patel] : Added a program of sum of two numbers

---
+++ gcr-codebase/program-prerequisites/Sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Sum
+{
+ public static void main(String args[])
+{
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Enter Number 1: ");
+  int n1 = sc.nextInt();
+  System.out.println("Enter Number 2: ");
+  int n2 = sc.nextInt();
+  int sum = n1+n2;
+  System.out.println(sum);
+  
+}
+}
