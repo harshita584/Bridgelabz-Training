0727819ca78876835d3bd37fd8174ae9b537f63f
harshita584 <harshitapatel472@gmail.com>
2025-12-19 12:51:32 +0530

    [Harshita Patel] : added a program to print student with age

---
+++ gcr-codebase/arrays/level-one/StudentCanVote.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class StudentCanVote {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// Defining an array of 10 integers
+		int arr[] = new int[10];
+	
+		// Taking user input
+		for(int i=0; i<10; i++)
+		{
+			System.out.println("Enter the element: " + (i+1)); 
+			arr[i] = sc.nextInt();
+		}
+		
+		// Printing whether a person can vote or not
+		for(int i=0; i<arr.length-1; i++)
+		{
+			if(arr[i] <= 0)
+			{
+				System.out.println("Invalid age.");
+			}
+			else if (arr[i] >= 18)
+			{
+				System.out.println("The student with the age " + arr[i] + " can vote.");
+			}
+			else 
+			{
+				System.out.println("The student with the age " + arr[i] + " cannot vote.");
+			}
+		}		
+	}
+}
