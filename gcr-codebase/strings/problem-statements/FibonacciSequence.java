85c8ad8bd4f6816fade2154995e93e4eaab4012e
harshita584 <harshitapatel472@gmail.com>
2025-12-23 23:37:49 +0530

    [Harshita Patel] : added a program to generate the fibonacci sequence

---
+++ gcr-codebase/strings/problem-statements/FibonacciSequence.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class FibonacciSequence {
+
+    // Function to generate Fibonacci sequence
+    static void printFibonacci(int terms) {
+        int a = 0, b = 1;
+
+        System.out.println("Fibonacci Sequence:");
+        for (int i = 1; i <= terms; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of terms: ");
+        int terms = sc.nextInt();
+
+        printFibonacci(terms);
+    }
+}
