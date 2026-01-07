18ec4e73e474c1f4643d8665355c66f1c3612fbe
harshita584 <harshitapatel472@gmail.com>
2026-01-07 14:56:19 +0530

    [Harshita Patel] : added a program to concatenate strings using string buffer

--- java-dsa-practice/gcr-codebase/datastructurepractice/src/com/linearandbinarysearch/ConcatenateStringsUsingStringBuffer.java
+++ java-dsa-practice/gcr-codebase/datastructurepractice/src/com/linearandbinarysearch/ConcatenateStringsUsingStringBuffer.java
@@ -2,4 +2,22 @@ package com.linearandbinarysearch;
 
 public class ConcatenateStringsUsingStringBuffer {
 
+    public static void main(String[] args) {
+
+        String[] words = {"Java", " ", "is", " ", "easy"};
+
+        // create StringBuffer object
+        StringBuffer sb = new StringBuffer();
+
+        // append each string to StringBuffer
+        for (int i = 0; i < words.length; i++) {
+            sb.append(words[i]);
+        }
+
+        // convert to String
+        String result = sb.toString();
+
+        // display output
+        System.out.println("Concatenated String: " + result);
+    }
 }
