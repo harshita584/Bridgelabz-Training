e33851eb4a02cbe01c7c14053723cd1d0cce8543
harshita584 <harshitapatel472@gmail.com>
2026-01-07 15:00:32 +0530

    [Harshita Patel] : added a program to find first negative number by linear search

--- java-dsa-practice/gcr-codebase/datastructurepractice/src/com/linearandbinarysearch/FirstNegativeLinearSearch.java
+++ java-dsa-practice/gcr-codebase/datastructurepractice/src/com/linearandbinarysearch/FirstNegativeLinearSearch.java
@@ -2,4 +2,29 @@ package com.linearandbinarysearch;
 
 public class FirstNegativeLinearSearch {
 
+    // method to find first negative number
+    static int findFirstNegative(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                return i; // return index of first negative number
+            }
+        }
+
+        return -1; // no negative number found
+    }
+
+    public static void main(String[] args) {
+
+        int[] numbers = {12, 5, 0, 9, -3, -7, 8};
+
+        int index = findFirstNegative(numbers);
+
+        if (index != -1) {
+            System.out.println("First negative number found at index: " + index);
+            System.out.println("Value: " + numbers[index]);
+        } else {
+            System.out.println("No negative number found in the array.");
+        }
+    }
 }
