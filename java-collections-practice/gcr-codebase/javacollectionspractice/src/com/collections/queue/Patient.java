875fdb4a6b83c75ef44f94f0b2f5f542e6418479
harshita584 <harshitapatel472@gmail.com>
2026-01-20 16:10:42 +0530

    [Harshita Patel] : added a program on hospital triage system

---
+++ java-collections-practice/gcr-codebase/javacollectionspractice/src/com/collections/queue/Patient.java
@@ -0,0 +1,15 @@
+package com.collections.queue;
+
+public class Patient {
+	String name;
+	int priority;
+	
+	public Patient(String name, int priority) {
+		this.name = name;
+		this.priority = priority;
+	}
+	
+	public String toString() {
+		return name + " " + priority;
+	}
+}
