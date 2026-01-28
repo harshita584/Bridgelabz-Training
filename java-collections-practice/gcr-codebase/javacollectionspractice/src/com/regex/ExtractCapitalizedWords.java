b105be87999de69078116ac86b306cef04d88c26
harshita584 <harshitapatel472@gmail.com>
2026-01-28 16:29:06 +0530

    [Harshita Patel] : added a program to extract capitalized words

---
+++ java-collections-practice/gcr-codebase/javacollectionspractice/src/com/regex/ExtractCapitalizedWords.java
@@ -0,0 +1,16 @@
+package com.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractCapitalizedWords {
+	public static void main(String[] args) {
+		String text = "The Eiffel Tower is in Paris and the Statue of Liberty is in New York.";
+	    Pattern pattern = Pattern.compile("\\b[A-Z][a-z]*\\b");
+	    Matcher matcher = pattern.matcher(text);
+
+	    while (matcher.find()) {
+	    	System.out.println(matcher.group());
+	    }
+	}
+}
