13b83e66dd0cdf179697afed982cfe80b565edf9
harshita584 <harshitapatel472@gmail.com>
2026-02-02 12:07:41 +0530

    [Harshita Patel] : added a program to use @override correctly

---
+++ java-collections-practice/gcr-codebase/javacollectionspractice/src/com/annotation/override/Dog.java
@@ -0,0 +1,13 @@
+package com.annotation.override;
+
+public class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Dog barks");
+    }
+    
+    public static void main(String[] args) {
+		Dog dog = new Dog();
+		dog.makeSound();
+	}
+}
